27b12100d7c0bf947ac6defa3a16798638752ca2
The Photographer Girl <jb.atlondon@gmail.com>
2025-12-28 05:18:02 +0000

    Update reputation.ts

--- src/commands/admin/reputation.ts
+++ src/commands/admin/reputation.ts
@@ -86,7 +86,7 @@ const reputationCommand: IApplicationCommand = {
         const targetId = interaction.options.getString('staff', true);
         const reason = interaction.options.getString('reason', true);
         const executor = interaction.member as GuildMember;
-        const staffRoleIds = staffConfig.roles.staffHerarchy.map((r: any) => r.id);
+        const staffRoleIds = staffConfig.roles.staffHierarchy.map((r: any) => r.id);
 
         try {
             if (targetId === interaction.user.id) {
